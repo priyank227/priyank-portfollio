9fe3d0ec2460a6648fcdd9b14100d7d17cdd4380
priyank viradiya <priyankviradiya227@gmail.com>
2024-04-20 22:34:54 +0530

    updated version

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// typing animation
+var typed =  new Typed(".typing" , {
+    strings:["","Web Developer","App developer","Graphic designer","Web Designer"],
+    typeSpeed:100,
+    BackSpeed:50,
+    loop:true
+})
+
+const nav = document.querySelector(".nav"),
+    navlist = nav.querySelectorAll("li"),
+    totalNavList = navlist.length;
+
+    const navTogglerBtn = document.querySelector('.nav-toggler'),
+        aside = document.querySelector(".aside");
+        navTogglerBtn.addEventListener("click",() => {
+            asideSectionTogglerBtn();
+        })
+        function asideSectionTogglerBtn()
+        {
+            aside.classList.toggle("open");
+            navTogglerBtn.classList.toggle("open");
+            for(let i=0; i<totalSection;i++){
+                allSection[i].classList.toggle("open");
+            }
+        }
