3682becd6f93c6437a6cd7713050e44f969cf528
priyank viradiya <priyankviradiya227@gmail.com>
2024-04-26 00:08:23 +0530

    update portfollio

--- js/script.js
+++ js/script.js
@@ -1,25 +1,42 @@
 // typing animation
-var typed =  new Typed(".typing" , {
-    strings:["","Web Developer","App developer","Graphic designer","Web Designer"],
-    typeSpeed:100,
-    BackSpeed:50,
-    loop:true
-})
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Web Developer",
+    "App developer",
+    "Graphic designer",
+    "Web Designer",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 50,
+  loop: true,
+});
 
 const nav = document.querySelector(".nav"),
-    navlist = nav.querySelectorAll("li"),
-    totalNavList = navlist.length;
+  navlist = nav.querySelectorAll("li"),
+  totalNavList = navlist.length;
 
-    const navTogglerBtn = document.querySelector('.nav-toggler'),
-        aside = document.querySelector(".aside");
-        navTogglerBtn.addEventListener("click",() => {
-            asideSectionTogglerBtn();
-        })
-        function asideSectionTogglerBtn()
-        {
-            aside.classList.toggle("open");
-            navTogglerBtn.classList.toggle("open");
-            for(let i=0; i<totalSection;i++){
-                allSection[i].classList.toggle("open");
-            }
-        }
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+  aside = document.querySelector(".aside");
+navTogglerBtn.addEventListener("click", () => {
+  asideSectionTogglerBtn();
+});
+function asideSectionTogglerBtn() {
+  aside.classList.toggle("open");
+  navTogglerBtn.classList.toggle("open");
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.toggle("open");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var links = document.querySelectorAll(".nav li a");
+  links.forEach(function (link) {
+    link.addEventListener("click", function () {
+      links.forEach(function (l) {
+        l.classList.remove("active");
+      });
+      this.classList.add("active");
+    });
+  });
+});
